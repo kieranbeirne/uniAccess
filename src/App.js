88ac64b1bf47439888da3a31c352d9c79270329a
kieranbeirne <kieranbeirne01@gmail.com>
2023-05-02 20:58:02 -0500

    added base template without content

--- src/App.js
+++ src/App.js
@@ -1,37 +1,106 @@
-import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import HomePage from "./HomePage";
-import AboutPage from "./AboutPage";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">My Portfolio</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+<div class="text">
+<section class="wrapper">
+  <div class="top">Team FoFoFo</div>
+  <div class="bottom" aria-hidden="true">Team FoFoFo</div>
+</section>
+  
+</div>
+      <header className="text-center my-5">
+      
+  
+  <p className="lead">Welcome to our CSCE 436 portfolio</p>
 
-      <footer className="footer mt-auto py-3 bg-light">
-        <div className="container">
-          <span className="text-muted">Place sticky footer content here.</span>
-        </div>
-      </footer>
+      </header>
+
+      <main className="container-lg">
+        <section className="section my-5" id="intro">
+          <h2 className="display-4">Introduction and Motivation</h2>
+          <p>Your content goes here...</p>
+        </section>
+
+        <section className="section my-5" id="video">
+          <h2 className="display-4">YouTube Promotion Video</h2>
+          <div className="embed-responsive embed-responsive-16by9">
+          <iframe width="960" height="569" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+        </section>
+
+        <section className="section my-5" id="slides">
+          <h2 className="display-4">Presentation Slides</h2>
+          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSLlnq4AtmNNqMJkUr2N1BJlDoxVeO-Iq9iHPn48lxCED27-xfvyLxrsWzDIedBt3Jr3agMAGodRV_z/embed?start=false&loop=false&delayms=30000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+        </section>
+
+        <section className="section my-5" id="feedback">
+          <h2 className="display-4">Key Insights from Peer Feedback</h2>
+          <p>Your content goes here...</p>
+        </section>
+
+        <section className="section my-5" id="conclusion">
+          <h2 className="display-4">Conclusion and Future Work</h2>
+          <p>Your content goes here...</p>
+        </section>
+
+        <div class="">
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <filter id="motion-blur-filter" filterUnits="userSpaceOnUse">
+       </filter>
+    </svg>
+    <span  >Kieran Beirne</span>
+  </div>
+
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <filter id="motion-blur-filter" filterUnits="userSpaceOnUse">
+       </filter>
+    </svg>
+    <span  >James Evans</span>
+  </div>
+</div>
+
+<div class="">
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <filter id="motion-blur-filter" filterUnits="userSpaceOnUse">
+       </filter>
+    </svg>
+    <span  >Adidev Mohaptra</span>
+  </div>
+
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <filter id="motion-blur-filter" filterUnits="userSpaceOnUse">
+        
+      </filter>
+    </svg>
+    <span  >Sean Kelly</span>
+  </div>
+</div>
+
+<div class="">
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+    </svg>
+    <span  >Hunter Zacha</span>
+  </div>
+
+  <div class="">
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <filter id="motion-blur-filter" filterUnits="userSpaceOnUse">
+       </filter>
+    </svg>
+    <span>Keegan Choudary</span>
+  </div>
+</div>
+
+      </main>
     </div>
   );
 }
